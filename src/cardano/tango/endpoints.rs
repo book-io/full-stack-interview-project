08e1726b70c3f5a9caf59e3e59bd32398f2329b4
gmoratorio <moratorio.guillermo@gmail.com>
2023-01-07 20:19:59 -0700

    GH-5 -- refactored template strings to be more readable, renamed endpoints to differentiate get_all_addresses

--- src/cardano/tango/endpoints.rs
+++ src/cardano/tango/endpoints.rs
@@ -2,14 +2,22 @@ pub fn add_cursor_to_url(url: &str, cursor: &str) -> String {
     format!("{}?cursor={}", url, cursor).to_string()
 }
 
-pub fn generate_get_all_addresses_endpoint(
+pub fn generate_get_all_addresses_for_stake_address_endpoint(
     base_url: &str,
     app_id: &str,
     stake_address: &str,
 ) -> String {
-    format!(
-        "{}/{}/v1/wallets/{}/addresses",
-        base_url, app_id, stake_address
-    )
-    .to_string()
+    format!("{base_url}/{app_id}/v1/wallets/{stake_address}/addresses").to_string()
+}
+
+pub fn generate_get_all_assets_endpoint(base_url: &str, app_id: &str, address: &str) -> String {
+    format!("{base_url}/{app_id}/v1/addresses/{address}/assets").to_string()
+}
+
+pub fn generate_get_all_addresses_for_asset_endpoint(
+    base_url: &str,
+    app_id: &str,
+    asset_id: &str,
+) -> String {
+    format!("{base_url}/{app_id}/v1/assets/{asset_id}/addresses").to_string()
 }
