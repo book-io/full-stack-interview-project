b1f2309865c15caf1f23a31c8f334dcfd4508702
gmoratorio <moratorio.guillermo@gmail.com>
2023-01-07 19:05:36 -0700

    GH-5 -- abstracted call to Tango into a generalized get_collection_from_tango

--- src/cardano/tango/client.rs
+++ src/cardano/tango/client.rs
@@ -61,6 +61,33 @@ where
     Ok(data)
 }
 
+/// Helper method that abstracts calls to Tango Crypto for an arbitrary url,
+/// which can optionally have a cursor appended
+async fn get_collection_from_tango<T>(
+    url: &str,
+    api_key: &str,
+    cursor: Option<String>,
+) -> anyhow::Result<ApiListRes<T>>
+where
+    T: DeserializeOwned,
+{
+    let full_url = if cursor.is_some() {
+        add_cursor_to_url(url, &cursor.unwrap())
+    } else {
+        url.to_string()
+    };
+
+    let res = reqwest::Client::new()
+        .get(full_url)
+        .header(TANGO_API_KEY_HEADER, api_key)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(res)
+}
+
 #[async_trait::async_trait]
 impl CardanoApi for TangoClient {
     // recommended api:
@@ -69,29 +96,8 @@ impl CardanoApi for TangoClient {
         let url = generate_get_all_addresses_endpoint(&self.base_url, &self.app_id, stake_address);
         let api_key = &self.api_key;
 
-        async fn get_addresses(
-            url: &str,
-            api_key: &str,
-            cursor: Option<String>,
-        ) -> anyhow::Result<ApiListRes<Address>> {
-            let full_url = if cursor.is_some() {
-                add_cursor_to_url(url, &cursor.unwrap())
-            } else {
-                url.to_string()
-            };
-
-            let res = reqwest::Client::new()
-                .get(full_url)
-                .header(TANGO_API_KEY_HEADER, api_key)
-                .send()
-                .await?
-                .json()
-                .await?;
-
-            Ok(res)
-        }
-
-        let response: Vec<Address> = get_all(|cursor| get_addresses(&url, api_key, cursor)).await?;
+        let response: Vec<Address> =
+            get_all(|cursor| get_collection_from_tango(&url, api_key, cursor)).await?;
 
         let addresses: Vec<String> = response
             .into_iter()
