a213864d18e54682bf3d561c5c7436b6f9fafeb7
gmoratorio <moratorio.guillermo@gmail.com>
2023-01-08 17:44:31 -0700

    GH-5 -- refactored template string to be more readable, removed unnecessary to_string calls

--- src/cardano/tango/endpoints.rs
+++ src/cardano/tango/endpoints.rs
@@ -1,5 +1,5 @@
 pub fn add_cursor_to_url(url: &str, cursor: &str) -> String {
-    format!("{}?cursor={}", url, cursor).to_string()
+    format!("{url}?cursor={cursor}")
 }
 
 pub fn generate_get_all_addresses_for_stake_address_endpoint(
@@ -7,11 +7,11 @@ pub fn generate_get_all_addresses_for_stake_address_endpoint(
     app_id: &str,
     stake_address: &str,
 ) -> String {
-    format!("{base_url}/{app_id}/v1/wallets/{stake_address}/addresses").to_string()
+    format!("{base_url}/{app_id}/v1/wallets/{stake_address}/addresses")
 }
 
 pub fn generate_get_all_assets_endpoint(base_url: &str, app_id: &str, address: &str) -> String {
-    format!("{base_url}/{app_id}/v1/addresses/{address}/assets").to_string()
+    format!("{base_url}/{app_id}/v1/addresses/{address}/assets")
 }
 
 pub fn generate_get_all_addresses_for_asset_endpoint(
@@ -19,5 +19,5 @@ pub fn generate_get_all_addresses_for_asset_endpoint(
     app_id: &str,
     asset_id: &str,
 ) -> String {
-    format!("{base_url}/{app_id}/v1/assets/{asset_id}/addresses").to_string()
+    format!("{base_url}/{app_id}/v1/assets/{asset_id}/addresses")
 }
